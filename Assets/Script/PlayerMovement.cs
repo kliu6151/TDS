f80dd811703f1fd11ec38a8ac2076d73499f108d
kliu6151 <kliu6151@bths.edu>
2019-02-17 12:16:21 -0500

    Movement & camera follow

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour { 
+
+    public float moveSpeed;
+    private Rigidbody playerRigidbody;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        playerRigidbody.velocity = moveVelocity;
+    }
+}
