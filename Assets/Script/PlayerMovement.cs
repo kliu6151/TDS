f4bbd9b2ef180a15cdd2efd5c23d29c2ede96503
f <g>
2019-02-26 11:41:14 -0500

    Gun Follow

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour {
 
     private Vector3 moveInput;
     private Vector3 moveVelocity;
+    new Camera mainCamera;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +21,18 @@ public class PlayerMovement : MonoBehaviour {
     {
         moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         moveVelocity = moveInput * moveSpeed;
-    }
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
 
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 Look = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, Look, Color.blue);
+
+            transform.LookAt(new Vector3(Look.x, Look.y, Look.z));
+        }
+    }
     private void FixedUpdate()
     {
         playerRigidbody.velocity = moveVelocity;
