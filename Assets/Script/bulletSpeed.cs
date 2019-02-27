ae5fa7b31e08a5e78887232f4a41188f2b223537
Z Chen <zchen6464@bths.edu>
2019-02-27 11:10:35 -0500

    Add files via upload

---
+++ Assets/Script/bulletSpeed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletSpeed : MonoBehaviour
+{
+
+    public float speed;
+    public Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = transform.up * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
