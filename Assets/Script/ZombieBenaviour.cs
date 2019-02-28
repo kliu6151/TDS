0397c489ce97c20a6542b03d3250a1d33c8e01fc
BT_1N3_32 <1q>
2019-02-28 11:26:25 -0500

    screw this

---
+++ Assets/Script/ZombieBenaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieBenaviour : MonoBehaviour
+{
+    public UnityEngine.AI.NavMeshAgent agent;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
