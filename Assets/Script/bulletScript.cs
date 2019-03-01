045512e90520319625e00ff63f5eb184ec9197e7
Z Chen <zchen6464@bths.edu>
2019-03-01 10:37:27 -0500

    Add files via upload

--- Assets/Script/bulletScript.cs
+++ Assets/Script/bulletScript.cs
@@ -13,29 +13,28 @@ public class bulletScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(this.gameObject);
         //set speed to "speed" in the direction of "up"
         //the bullet moves where the pointy part is facing (given that it's rotate 90* in the x axis)
         rigidbody = GetComponent<Rigidbody>();
         rigidbody.velocity = transform.up * speed;
         
         //detects if the collision is with a enemy/damagable entity
-        if(other.tag == tag)
-        {
-            //use GetComponent to access the script and thus the health
-            health = other. GetComponent<Health>();
-            health.hp = health.hp - damage;
-            if(health.hp <=0)
-            {
-                Destroy(other.gameObject);
-            }
-        }
     }
     
     private void OnTriggerEnter(Collider other)
     {
         //destroys the bullet upon contact with another object/collider
         Destroy(this.gameObject);
+        if (other.tag == tag)
+        {
+            //use GetComponent to access the script and thus the health
+            health = other.GetComponent<Health>();
+            health.hp = health.hp - damage;
+            if (health.hp <= 0)
+            {
+                Destroy(other.gameObject);
+            }
+        }
     }
     
     // Update is called once per frame
