57c0a4548502c281fae45252f6094ecee900d0fc
BT_1N3_32 <1q>
2019-03-06 11:36:10 -0500

    wow!

---
+++ Assets/Script/StartButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.name == "Start")
+                {
+                    SceneManager.LoadScene("SampleScene");
+                }
+            }
+        }
+    }
+}
