4b5e32df22b2b3c5b42c2601ebee333acdf104cd
BT_1N3_32 <1q>
2019-03-07 10:48:19 -0500

    Created walls

---
+++ Assets/Script/NextLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int level = 1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindWithTag("Zombie") == null)
+        {
+            level++;
+            SceneManager.LoadScene(level);
+        }
+    }
+}
