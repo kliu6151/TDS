82319730abe8aa29ca01e7c1ba2eaae9db7f517b
BT_1N3_32 <1q>
2019-03-22 10:26:24 -0400

    hi

--- Assets/Script/SpawnerBehaviour.cs
+++ Assets/Script/SpawnerBehaviour.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 public class SpawnerBehaviour : MonoBehaviour
 {
     public float spawnerDelay = 5;
+    public System.Boolean active;
     private float countdown;
+    private System.Boolean activeTemp;
     // Start is called before the first frame update
     void Start()
     {
-        
+        activeTemp = active;
     }
 
     // Update is called once per frame
     void Update()
     {
-        countdown -= Time.deltaTime;
-        if (countdown <= 0)
+        if(!active && Vector3.Distance(GameObject.FindWithTag("Player").transform.position, transform.position) < 15)
         {
-            countdown = spawnerDelay;
-            GameObject Zombie = Instantiate(Resources.Load<GameObject>("Zombie"), transform.position, transform.rotation) as GameObject;
+            activeTemp = true;
+        }
+        else
+        {
+            activeTemp = false;
+        }
+        if (activeTemp)
+        {
+            countdown -= Time.deltaTime;
+            if (countdown <= 0)
+            {
+                countdown = spawnerDelay;
+                GameObject Zombie = Instantiate(Resources.Load<GameObject>("Zombie"), transform.position, transform.rotation) as GameObject;
+            }
         }
     }
 }
