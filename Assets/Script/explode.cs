fefdbcb3b6fc5545c3997899648246f626e8f3a9
Z Chen <zchen6464@bths.edu>
2019-03-29 11:44:07 -0400

    Update explode.cs

--- Assets/Script/explode.cs
+++ Assets/Script/explode.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,44 +6,53 @@ public class explode : MonoBehaviour
 {
     public float health = 1;
     public float explosionDmg = 50;
-	public float force;
+    public float force;
     public ParticleSystem particleSystem;
-	public Renderer render;
-	public Collider collider;
+    public Renderer render;
+    public Collider collider;
+    public Collider range;
     private bool oneTime = true;
     // Start is called before the first frame update
     public void Start()
     {
+        range.enabled = false;
     }
 
     public void OnTriggerStay(Collider other)
     {
         if(health <= 0)
-        {
+        { 
            if(oneTime)
            {
-              particleSystem.Play();
-              oneTime =  false;
-			 // GetComponent<Rigidbody>().AddExplosionForce(force, GetComponent<Rigidbody>().centerOfMass, 5, 1, ForceMode.Impulse);
-              if(other.GetComponent<Health>() != null)
-              {
-                other.GetComponent<Health>().takeDamage(explosionDmg);
-              }
+               particleSystem.Play();
+               oneTime =  false;
+               if(other.GetComponent<Health>() != null)
+               {
+                  other.GetComponent<Health>().takeDamage(explosionDmg);
+               }
+               range.enabled = false;
            }
         }
     }
-    public void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "bullet" || collision.gameObject.tag == "Barrel")
+        if (collision.gameObject.tag == "bullet")
         {
             health =- collision.gameObject.GetComponent<bulletScript>().damage;
         }
         if (health <= 0)
         {
-			render.enabled = false;
-			collider.enabled = false;
-            Destroy(gameObject, 4f);
+            boom();
         }
+    }
+
+    public void boom()
+    {
+        health = 0;
+        render.enabled = false;        
+        collider.enabled = false;
+        range.enabled = true;
+        Destroy(gameObject, 4f);
     }
 
 }
